830ad96ae480715d10f1a86515aae5c0a2d8e8bb
caioopra <caioprsilva@gmail.com>
2024-10-28 11:29:13 -0300

    (tests) feat: adding test for trying to add a vertex as neighbor multiple times

--- src/vertex.rs
+++ src/vertex.rs
@@ -62,7 +62,7 @@ mod tests {
     }
 
     #[test]
-    fn adding_neigbor() {
+    fn adding_neighbor() {
         let u = Vertex::new(0, String::from("TestU"));
         let v = Vertex::new(1, String::from("TestV"));
 
@@ -76,14 +76,44 @@ mod tests {
     }
 
     #[test]
+    fn adding_same_neighbor_twice() {
+        let u = Vertex::new(0, String::from("TestU"));
+        let v = Vertex::new(1, String::from("TestV"));
+
+        Vertex::add_neighbor(u.clone(), v.clone());
+        Vertex::add_neighbor(u.clone(), v.clone());
+        Vertex::add_neighbor(v.clone(), u.clone());
+
+        assert_eq!(u.borrow().degree, 1);
+        assert_eq!(u.borrow().neighbors.len(), 1);
+
+        assert_eq!(v.borrow().degree, 1);
+        assert_eq!(v.borrow().neighbors.len(), 1);
+    }
+
+    #[test]
     fn correct_amount_of_neighbors() {
         let a = Vertex::new(0, String::from("Test a"));
         let b = Vertex::new(1, String::from("Test b"));
-        let c = Vertex::new(1, String::from("Test c"));
-        let d = Vertex::new(1, String::from("Test d"));
-        let e = Vertex::new(1, String::from("Test e"));
+        let c = Vertex::new(2, String::from("Test c"));
+        let d = Vertex::new(3, String::from("Test d"));
+        let e = Vertex::new(4, String::from("Test e"));
 
         Vertex::add_neighbor(a.clone(), b.clone());
+        Vertex::add_neighbor(a.clone(), c.clone());
 
+        Vertex::add_neighbor(b.clone(), c.clone());
+
+        Vertex::add_neighbor(c.clone(), d.clone());
+
+        Vertex::add_neighbor(d.clone(), e.clone());
+        Vertex::add_neighbor(d.clone(), a.clone());
+        Vertex::add_neighbor(d.clone(), c.clone());
+
+        assert_eq!(a.borrow().degree, 3);
+        assert_eq!(b.borrow().degree, 2);
+        assert_eq!(c.borrow().degree, 3);
+        assert_eq!(d.borrow().degree, 3);
+        assert_eq!(e.borrow().degree, 1);
     }
 }
